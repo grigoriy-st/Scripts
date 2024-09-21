06b05be74ac9aa1e3c90ce54a2cdf810d63b9b84
gregorey0213 <gregorey060@gmail.com>
2024-09-21 10:50:48 +0600

    Added git script

---
+++ for_git.ps1
@@ -0,0 +1,19 @@
+﻿git init
+git add .
+git status
+$message = Read-Host "Enter text message:"
+git commit -m $message
+git status
+
+echo "Press enter to continue"
+$key = $host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
+if ($key.VirtualKeyCode -eq 13) {
+
+    Write-Host "Upload rep"
+}
+
+git push git@github.com:grigoriy-st/Scripts.git
+
+
+echo "SUCCESS"
+Start-Sleep -s 4
