bf7bdf876915c0e5779d5135d7a44f01bd2a081d
Grigoriy <gregorey060@gmail.com>
2025-10-10 18:43:59 +0600

    Add script for copy from local to repository

---
+++ Set_Up_System/config_files/config_sync.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+echo "Move config files"
+echo -e "Choose files to move:\n"
+
+files=(*)
+
+for index in "${!files[@]}"; do
+    if [[ "${files[index]}" != "$0" ]]; then
+        printf "%2d) %s\n" $index "${files[$index]}"
+    fi
+done
+
+read -p "Input number: " choice
+selected_item="${files[choice]}"
+
+case "$selected_item" in
+    vim)
+        cp ~/.vimrc ./vim
+        echo "vimrc is copied"
+    ;;
+    terminal\ emulators)
+        cp ~/.config/alacritty/alacritty.toml ./alacritty.toml
+        echo "alacritty is copied"
+    ;;
+# Coming soon...
+esac
+
+
